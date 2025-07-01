61dd9a51d2b93b0897a414675a778f30e72ddb8d
Lu Xun <vboxuser@RSE.myguest.virtualbox.org>
2025-07-01 15:50:05 +0800

    Initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /home/vboxuser/catkin_ws/src
+# Build directory: /home/vboxuser/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("turtlebot3_simulations/turtlebot3_simulations")
+subdirs("beginner_tutorials")
+subdirs("turtlebot3_simulations/turtlebot3_fake")
+subdirs("turtlebot3_simulations/turtlebot3_gazebo")
+subdirs("learning_tf2")
